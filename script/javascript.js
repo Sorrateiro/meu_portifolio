98ae70e03ce6f79bee246a46df16fdcbb53563ff
Henrique <151760683+Sorrateiro@users.noreply.github.com>
2026-02-10 17:27:54 -0300

    salve dois

--- script/javascript.js
+++ script/javascript.js
@@ -9,3 +9,23 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+  function menuclick() {
+    const navMenu = document.getElementById('nav-menu');
+    if (navMenu.style.display === 'block') {
+      navMenu.style.display = 'none';
+    } else {
+      navMenu.style.display = 'block';
+    }
+  }
+function escreva() {
+  const texto = document.getElementById("visualização");
+  const display = window.innerWidth;
+  if (display < 768) {
+    texto.innerHTML = "Visualização Mobile";
+  } else {
+    texto.innerHTML = "Visualização Desktop";
+  }
+  
+}
+window.addEventListener("resize", escreva);
+window.addEventListener("load", escreva);
